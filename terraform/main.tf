3cec143590ab110ae78b9c03bbcdb07bef7d65e9
KibouAkari <simeon.schaer1@gmail.com>
2025-06-20 15:41:58 +0200

    Enhance CI/CD pipeline with Terraform deployment and OWASP ZAP scan; update resource configurations and remove obsolete files.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,38 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "nicolas-test"
+  name     = "juice-shop-rg"
   location = var.location
+
   tags = {
-    created_by    = "nicolas"
-    creation_date = "12.06.25"
+    created_by    = "KibouAkari"
+    creation_date = "20.06.25"
   }
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "nicolasContainerRegistry8726422"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_container_group" "juice_shop" {
+  name                = "juice-shop-container"
   location            = var.location
-  sku                 = "Basic"
-  admin_enabled       = true
-  tags = {
-    created_by    = "nicolas"
-    creation_date = "12.06.25"
-  }
-}
-
-resource "azurerm_container_group" "continst" {
-  name = "nicolas-continst"
-  location = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type = "Public"
-  dns_name_label = "aci-label"
-  os_type = "Linux"
+  ip_address_type     = "Public"
+  dns_name_label      = "juice-shop-${random_id.dns.hex}"
+  os_type             = "Linux"
+
   container {
-    name = "nicolas-container"
-    
-    image = "https://hub.docker.com/r/bkimminich/juice-shop:latest"
-    cpu = "0.5"
+    name   = "juice-shop"
+    image  = "bkimminich/juice-shop:latest"
+    cpu    = "0.5"
     memory = "1.5"
+
     ports {
-      port = 3000
+      port     = 3000
       protocol = "TCP"
     }
-
   }
 
   tags = {
-    environment = "nicolas-testing"
+    environment = "dev"
   }
+}
 
+resource "random_id" "dns" {
+  byte_length = 4
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 variable "subscription_id" {
   description = "Subscription ID"
   type        = string
-  default     = "b9eb2065-47fa-47ff-8e7b-e4dc39a2103f"
+  default     = "subscription_id"
 }
 variable "tenant_id" {
   description = "Tenant ID"
   type        = string
-  default     = "4e86a400-8622-4b45-bfb2-565ddc9d1048"
+  default     = "tenant_id"
 }
 
 variable "location" {
